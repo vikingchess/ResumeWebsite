94f160e48af0adca74aeb24d260e9e8dbbef6bea
DESKTOP-O8GP7PU\David <davidflom01@gmail.com>
2017-10-24 09:29:35 -0400

    Initial Commit 10 24 17

---
+++ main.js
@@ -0,0 +1,50 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCwC7U1GrTjk6-ZVScaUDN3nX_fobxuINI",
+    authDomain: "contactform-7bb34.firebaseapp.com",
+    databaseURL: "https://contactform-7bb34.firebaseio.com",
+    projectId: "contactform-7bb34",
+    storageBucket: "contactform-7bb34.appspot.com",
+    messagingSenderId: "468892424814"
+  };
+  firebase.initializeApp(config);
+
+//conect to firebase messages collections
+var messageRef = firebase.database().ref('messages');
+
+//event listener for form
+document.getElementById('contactForm').addEventListener('submit', submitForm);
+function submitForm(e){
+    e.preventDefault();
+    //getvalues
+    var name = getInputVal('name');
+    var company = getInputVal('company');
+    var email = getInputVal('email');
+    var phone = getInputVal('phone');
+    var message = getInputVal('message');
+    sendMessage(name,company, email, phone, message);
+    //display alert
+    document.querySelector('.alert').style.display = 'block';
+    //hide alert of 3 seconds
+    setTimeout(function(){
+        document.querySelector('.alert').style.display = 'none'; 
+        document.getElementById('contactForm').reset();
+    },3000);
+}    
+//Function to get form values
+function getInputVal(id){
+    return document.getElementById(id).value;
+}
+
+//send the message to firebase
+function sendMessage(name, company, email, phone, message){
+    var newMessage = messageRef.push();
+    newMessage.set({
+        name:name,
+        company:company,
+        email:email,
+        phone:phone,
+        message:message
+    });
+}
